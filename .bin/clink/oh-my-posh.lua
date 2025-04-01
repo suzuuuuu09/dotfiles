ea6c031a31b3846f5bea9351bfbedafeb2a316b3
suzuuuuu09 <140299273+suzuuuuu09@users.noreply.github.com>
2025-04-02 01:38:44 +0900

    update oh-my-posh.lua

--- .bin/clink/oh-my-posh.lua
+++ .bin/clink/oh-my-posh.lua
@@ -1,1 +1,1 @@
-load(io.popen('oh-my-posh init cmd --config "%POSH_THEMES_PATH%\\dracula-detailed.omp.json"'):read("*a"))()
+load(io.popen('oh-my-posh init cmd --config "%POSH_THEMES_PATH%\\nord-detailed.omp.json"'):read("*a"))()
