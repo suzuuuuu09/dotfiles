fdda93cab4a894d6ace50b1f1758c93e500c7698
suzuuuuu09 <140299273+suzuuuuu09@users.noreply.github.com>
2026-02-25 02:52:43 +0900

    Update

---
+++ README.md
@@ -0,0 +1,7 @@
+# suzuuuuu09's Dotfiles
+
+## How to setup?
+- `chezmoi` を使います。インストールしていない場合は[インストールガイド](https://www.chezmoi.io/install/)を参考にしてください。
+```sh
+sh -c "$(curl -fsLS get.chezmoi.io)" -- init --apply suzuuuuu09
+```
--- run_once_setup-symlinks.sh
+++ run_once_setup-symlinks.sh
@@ -1,11 +1,26 @@
 #!/bin/sh
 
-CHEZMOI_SOURCE="$HOME/.local/share/chezmoi"
+CHEZMOI_CONFIG_DIR="$HOME/.local/share/chezmoi/dot_config"
 CONFIG_DIR="$HOME/.config"
-APPS="nvim wezterm fish"
 
-for app in $APPS; do
-	  echo "Setting up symlink for $app..."
-    rm -rf "$CONFIG_DIR/$app"
-    ln -s "$CHEZMOI_SOURCE/dot_config/$app" "$CONFIG_DIR/$app"
+# NOTE: dot_config内にあるフォルダだけ書く
+APPS=(
+		"nvim"
+		"wezterm"
+		"fish"
+		"gh"
+		"zsh"
+		"oh-my-posh"
+)
+
+for app in "${APPS[@]}"; do
+    echo "Setting up symlink for $app..."
+    
+    # 実体があるか確認（Neovimなどで設定ミスを防ぐため）
+    if [ -d "$CHEZMOI_CONFIG_DIR/$app" ]; then
+        rm -rf "$CONFIG_DIR/$app"
+        ln -s "$CHEZMOI_CONFIG_DIR/$app" "$CONFIG_DIR/$app"
+    else
+        echo "Error: Not found $CHEZMOI_CONFIG_DIR/$app"
+    fi
 done
