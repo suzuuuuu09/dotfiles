c803cde8c0a9859821f3efdb6ac96767a959a25f
suzuuuuu09 <140299273+suzuuuuu09@users.noreply.github.com>
2026-04-05 00:04:23 +0900

    ✨comment-boxのキーを変更 (nvim)

--- .config/nvim/lua/plugins/comment/comment-box.lua
+++ .config/nvim/lua/plugins/comment/comment-box.lua
@@ -3,8 +3,8 @@ return {
 	event = { "BufReadPre", "BufNewFile" },
 	config = true,
 	keys = {
-		{ mode = { "n", "v" }, "gcbt", "<CMD>CBlcbox<CR>",  desc = "Box title" },
-		{ mode = { "n", "v" }, "gcbl", "<CMD>CBllline<CR>", desc = "Title line" },
-		{ mode = { "n", "v" }, "gcbd", "<CMD>CBd<CR>",      desc = "Remove comment box" },
+		{ mode = { "n", "v" }, "gcbt", "<CMD>CBlcbox10<CR>", desc = "Box title" },
+		{ mode = { "n", "v" }, "gcbl", "<CMD>CBllline<CR>",  desc = "Title line" },
+		{ mode = { "n", "v" }, "gcbd", "<CMD>CBd<CR>",       desc = "Remove comment box" },
 	},
 }
