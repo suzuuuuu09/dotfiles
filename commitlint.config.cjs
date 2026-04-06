a2289aa3e68c4c1fe178bb04b25b25e7779e08ee
suzuuuuu09 <140299273+suzuuuuu09@users.noreply.github.com>
2026-04-06 22:05:33 +0900

    🔧 commitlintの設定追加 (czg)

---
+++ commitlint.config.cjs
@@ -0,0 +1,154 @@
+module.exports = {
+	rules: {
+		// @see: https://commitlint.js.org/#/reference-rules
+	},
+	prompt: {
+		alias: { fd: "docs: fix typos" },
+		messages: {
+			type: "変更の種類を選択してください:",
+			scope: "変更のスコープを入力してください（任意）:",
+			customScope: "変更のスコープを入力してください:",
+			subject: "変更内容を短く命令形で記述してください:\n",
+			body: "変更内容をより詳しく記述してください（任意）。改行する場合は「|」を使用してください:\n",
+			breaking:
+				"破壊的変更がある場合は記述してください（任意）。改行する場合は「|」を使用してください:\n",
+			footerPrefixesSelect:
+				"この変更に関連するISSUEの種類を選択してください（任意）:",
+			customFooterPrefix: "ISSUEのプレフィックスを入力してください:",
+			footer: "関連するISSUEを記述してください。例: #4, #15:\n",
+			generatingByAI: "AIがコミットメッセージを生成中...",
+			generatedSelectByAI: "AIが生成した候補から選択してください:",
+			confirmCommit: "上記の内容でコミットを実行しますか？",
+		},
+
+		types: [
+			{
+				value: "✨",
+				name: "✨  新機能の追加",
+				emoji: "✨",
+				title: "Features",
+				description: "Add new features to the codebase.",
+			},
+			{
+				value: "⚡️",
+				name: "⚡️  パフォーマンスの改善",
+				emoji: "⚡️",
+				title: "Performance Improvements",
+				description: "Improve the performance of the codebase.",
+			},
+			{
+				value: "🐛",
+				name: "🐛  バグ修正",
+				emoji: "🐛",
+				title: "Bug Fixes",
+				description: "Fix bugs in the codebase.",
+			},
+			{
+				value: "🩹",
+				name: "🩹  重要ではない軽微な問題の修正",
+				emoji: "🩹",
+				title: "Minor Fixes",
+				description: "Fix minor issues that are not critical.",
+			},
+			{
+				value: "♻️",
+				name: "♻️   コードのリファクタリング",
+				emoji: "♻️",
+				title: "Refactoring",
+				description: "Refactor code without changing its behavior.",
+			},
+			{
+				value: "🔥",
+				name: "🔥  コード/ファイルの削除",
+				emoji: "🔥",
+				title: "Code/Files Removal",
+				description: "Remove code or files from the codebase.",
+			},
+			{
+				value: "🎉",
+				name: "🎉  プロジェクトの開始",
+				emoji: "🎉",
+				title: "Initial Commit",
+				description: "Initial commit to start the project.",
+			},
+			{
+				value: "🚨",
+				name: "🚨  コンパイラ/リンターのエラー修正",
+				emoji: "🚨 ",
+				title: "Compiler/Linter Fixes",
+				description: "Fix compiler or linter errors in the codebase.",
+			},
+			{
+				value: "🎨",
+				name: "🎨  コードの構造/フォーマットを改善",
+				emoji: "🎨",
+				title: "Code Style Improvements",
+				description:
+					"Improve the structure or format of the code without changing its behavior.",
+			},
+			{
+				value: "🔧",
+				name: "🔧  設定ファイルの追加/更新",
+				emoji: "🔧 ",
+				title: "Configuration",
+				description: "Add or update configuration files.",
+			},
+			{
+				value: "🚧",
+				name: "🚧  作業中",
+				emoji: "🚧",
+				title: "Work in Progress",
+				description: "Work in progress, not ready for review or merging.",
+			},
+			{
+				value: "📝",
+				name: "📝  ドキュメントの追加/更新",
+				emoji: "📝",
+				title: "Documentation",
+				description: "Add or update documentation.",
+			},
+			{
+				value: "📦",
+				name: "📦  パッケージの追加/更新",
+				emoji: "📦",
+			},
+		],
+		useEmoji: true,
+		emojiAlign: "center",
+		useAI: false,
+		aiNumber: 1,
+		themeColorCode: "",
+		scopes: [],
+		allowCustomScopes: true,
+		allowEmptyScopes: true,
+		customScopesAlign: "bottom",
+		customScopesAlias: "custom",
+		emptyScopesAlias: "empty",
+		upperCaseSubject: false,
+		markBreakingChangeMode: false,
+		allowBreakingChanges: ["feat", "fix"],
+		breaklineNumber: 100,
+		breaklineChar: "|",
+		skipQuestions: [],
+		issuePrefixes: [
+			{ value: "closed", name: "closed:   ISSUES has been processed" },
+		],
+		customIssuePrefixAlign: "top",
+		emptyIssuePrefixAlias: "skip",
+		customIssuePrefixAlias: "custom",
+		allowCustomIssuePrefix: true,
+		allowEmptyIssuePrefix: true,
+		confirmColorize: true,
+		scopeOverrides: undefined,
+		defaultBody: "",
+		defaultIssues: "",
+		defaultScope: "",
+		defaultSubject: "",
+		formatMessageCB: ({ defaultMessage, type, scope, subject }) => {
+			if (scope) {
+				return `${type} ${subject} (${scope})`;
+			}
+			return `${type} ${subject}`;
+		},
+	},
+};
