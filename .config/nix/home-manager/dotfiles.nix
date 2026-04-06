b3ce4cc53225e412360c38c8834bc91f2c23e961
suzuuuuu09 <140299273+suzuuuuu09@users.noreply.github.com>
2026-04-06 23:25:08 +0900

    🔧 aeroSpaceの初期設定を追加 (aerospace)

--- .config/nix/home-manager/dotfiles.nix
+++ .config/nix/home-manager/dotfiles.nix
@@ -7,6 +7,7 @@ in {
     # +----------------------------------------------------------+
     # |                       ディレクトリ                       |
     # +----------------------------------------------------------+
+    "aerospace".source = mkLink ".config/aerospace";
     "oh-my-posh".source = mkLink ".config/oh-my-posh";
     "nix".source = mkLink ".config/nix";
     "nvim".source = mkLink ".config/nvim";
