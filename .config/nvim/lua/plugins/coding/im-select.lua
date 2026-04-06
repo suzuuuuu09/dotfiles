8b815c8af912780107c661c9ae174083d619deb8
suzuuuuu09 <140299273+suzuuuuu09@users.noreply.github.com>
2026-04-06 22:04:49 +0900

    ✨ im-selectの設定を直した (nvim)

--- .config/nvim/lua/plugins/coding/im-select.lua
+++ .config/nvim/lua/plugins/coding/im-select.lua
@@ -3,10 +3,10 @@
 return {
 	"keaising/im-select.nvim",
 	config = function()
-		--[[ require("im_select").setup({
+		require("im_select").setup({
 			default_im_select = "net.mtgto.inputmethod.macSKK.ascii",
 			default_command = "macism",
 			set_previous_im_on_default = true,
-		}) ]]
+		})
 	end,
 }
