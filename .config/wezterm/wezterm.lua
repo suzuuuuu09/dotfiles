9a67810879dc66f71bcfb820bcdf9bead91eb9ef
suzuuuuu09 <140299273+suzuuuuu09@users.noreply.github.com>
2026-05-09 01:48:50 +0900

    ✨ caps_lockをcontrolに変換し、WezTermのleaderキーも変更した (karabiner,wezterm)

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -113,6 +113,6 @@ end)
 config.disable_default_key_bindings = false
 config.keys = require("keybinds").keys
 config.key_tables = require("keybinds").key_tables
-config.leader = { key = "p", mods = "CTRL", timeout_milliseconds = 2000 }
+config.leader = { key = "q", mods = "CTRL", timeout_milliseconds = 2000 }
 
 return config
