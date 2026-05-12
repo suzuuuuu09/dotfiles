a8e4f6a6b6cb1f2bda356ee84005efc655d9afeb
suzuuuuu09 <140299273+suzuuuuu09@users.noreply.github.com>
2026-05-13 00:36:09 +0900

    🐛 marpのウォッチモードでの自動更新のため、設定を修正した (nvim)

--- .config/nvim/after/ftplugin/markdown.lua
+++ .config/nvim/after/ftplugin/markdown.lua
@@ -3,6 +3,6 @@ local opt = vim.opt
 -- 折り返し
 opt.wrap = true
 
--- NOTE: Marpのウォッチモードで保存時に更新されなかったため、更新されるようにするための設定
+-- NOTE: Marpのウォッチモードで保存時に更新するために必要
 -- 保存時にファイルをバックアップするようにする
 opt.backupcopy = "yes"
