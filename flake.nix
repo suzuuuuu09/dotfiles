30ab9308bf5a48351b3ee74e90eb7c2f4cad7df0
suzuuuuu09 <140299273+suzuuuuu09@users.noreply.github.com>
2026-06-17 09:18:54 +0900

    ⚡️ perf(nix): Nixキャッシュ設定を追加
    Nixの評価・ビルド時に使用するキャッシュの設定を追加した

--- flake.nix
+++ flake.nix
@@ -1,6 +1,19 @@
 {
   description = "suzuuuuu09 Nix configuration";
 
+  nixConfig = {
+    # このflakeの評価・ビルド時に使用するキャッシュ
+    extra-substituters = [
+      "https://suzuuuuu09.cachix.org"
+      "https://nix-community.cachix.org"
+    ];
+
+    extra-trusted-public-keys = [
+      "suzuuuuu09.cachix.org-1:+V6hB76qnQ1Ra3Lf9VZsQtszeZ9UyE39QvRHNtfYPXw="
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    ];
+  };
+
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
 
@@ -14,7 +27,7 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    # homebrew
+    # Homebrew
     nix-homebrew.url = "github:zhaofengli/nix-homebrew";
 
     sops-nix = {
@@ -27,9 +40,7 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    llm-agents = {
-      url = "github:numtide/llm-agents.nix";
-    };
+    llm-agents.url = "github:numtide/llm-agents.nix";
 
     # +----------------------------------------------------------+
     # | Agent Skills                                             |
@@ -40,34 +51,42 @@
       url = "path:./skills";
       flake = false;
     };
+
     vercel-skills = {
       url = "github:vercel-labs/skills";
       flake = false;
     };
+
     vercel-agent-skills = {
       url = "github:vercel-labs/agent-skills";
       flake = false;
     };
+
     anthropics-skills = {
       url = "github:anthropics/skills";
       flake = false;
     };
+
     agent-browser = {
       url = "github:vercel-labs/agent-browser";
       flake = false;
     };
+
     context7-skills = {
       url = "github:upstash/context7/574fc642ab4ba6e178b56f436928cb564349cf57";
       flake = false;
     };
+
     mattpocock-skills = {
       url = "github:mattpocock/skills";
       flake = false;
     };
+
     mizchi-skills = {
       url = "github:mizchi/skills/89c1b34018ba0d85a770c065ba22f83a8d004d34";
       flake = false;
     };
+
     ui-ux-pro-max-skills = {
       url = "github:nextlevelbuilder/ui-ux-pro-max-skill/b7e3af80f6e331f6fb456667b82b12cade7c9d35";
       flake = false;
@@ -80,12 +99,12 @@
     nixpkgs,
     home-manager,
     nix-homebrew,
-    agent-skills,
     ...
   } @ inputs: let
-    # TODO: 将来的にはArchlinuxもサポートする予定
+    # TODO: 将来的にはArch Linuxもサポートする予定
     targetSystem = "aarch64-darwin";
     isDarwin = nixpkgs.lib.hasSuffix "darwin" targetSystem;
+
     localOverlays = [
       (final: prev: {
         czg = prev.callPackage ./.config/nix/overlays/czg.nix {};
@@ -95,47 +114,67 @@
   in {
     darwinConfigurations."suzuMac" = nix-darwin.lib.darwinSystem {
       system = targetSystem;
-      specialArgs = {inherit self inputs;};
+
+      specialArgs = {
+        inherit self inputs;
+      };
+
       modules =
-        (nixpkgs.lib.optionals isDarwin [./.config/nix/darwin])
+        (nixpkgs.lib.optionals isDarwin [
+          ./.config/nix/darwin
+        ])
         ++ [
           nix-homebrew.darwinModules.nix-homebrew
           home-manager.darwinModules.home-manager
-          {
+
+          ({pkgs, ...}: {
             nixpkgs.overlays =
               [
                 inputs.llm-agents.overlays.default
               ]
               ++ localOverlays;
+
+            # Mac全体で永続的に使用するNixキャッシュ設定
+            nix.settings = {
+              extra-substituters = [
+                "https://suzuuuuu09.cachix.org"
+                "https://nix-community.cachix.org"
+              ];
+
+              extra-trusted-public-keys = [
+                "suzuuuuu09.cachix.org-1:+V6hB76qnQ1Ra3Lf9VZsQtszeZ9UyE39QvRHNtfYPXw="
+                "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+              ];
+
+              trusted-users = [
+                "root"
+                "k25012kk"
+              ];
+            };
+
+            # cachix pushなどのコマンドを使用可能にする
+            environment.systemPackages = with pkgs; [
+              cachix
+            ];
+
             home-manager = {
               useGlobalPkgs = true;
               useUserPackages = true;
               backupFileExtension = "backup";
-              extraSpecialArgs = {inherit inputs;};
+
+              extraSpecialArgs = {
+                inherit inputs;
+              };
+
               users.k25012kk = import ./.config/nix/home-manager;
+
               sharedModules = [
                 inputs.sops-nix.homeManagerModules.sops
                 inputs.nix-index-database.homeModules.nix-index
               ];
             };
-          }
+          })
         ];
     };
-  };
-
-  nixConfig = {
-    extra-substituters = [
-      "https://suzuuuuu09.cachix.org" # ここを自分のキャッシュ URL に変更
-      "https://nix-community.cachix.org" # ついでに nix-community も追加
-    ];
-    extra-trusted-public-keys = [
-      "suzuuuuu09.cachix.org-1:+V6hB76qnQ1Ra3Lf9VZsQtszeZ9UyE39QvRHNtfYPXw="
-      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-    ];
-    trusted-users = [
-      "root"
-      "@wheel"
-      "@admin"
-    ];
   };
 }
