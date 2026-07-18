8717063bb9c3867671685ea8d1c3e6158b8d7642
suzuuuuu09 <140299273+suzuuuuu09@users.noreply.github.com>
2026-07-18 19:06:31 +0900

    ✨ feat(nvim): flashプラグインの設定を更新
    モード設定を追加し、動作を拡張した

--- .config/nvim/lua/plugins/jump/flash.lua
+++ .config/nvim/lua/plugins/jump/flash.lua
@@ -1,6 +1,8 @@
 return {
 	"folke/flash.nvim",
 	event = { "BufReadPre", "BufNewFile" },
+	---@module "flash"
+	---@type Flash.Config
 	opts = {
 		modes = {
 			char = { enable = false },
@@ -11,6 +13,7 @@ return {
 	keys = {
 		{
 			"<CR>",
+			mode = { "n", "x", "o", "v" },
 			function()
 				require("flash").jump({
 					label = { before = true, after = false },
