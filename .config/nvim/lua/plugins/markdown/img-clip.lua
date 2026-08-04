fbc79c13e70d475016a2eea27bccf73761b61d3a
suzuuuuu09 <140299273+suzuuuuu09@users.noreply.github.com>
2026-08-04 22:19:27 +0900

    ✨ feat(nvim): add image clipboard paste plugin

---
+++ .config/nvim/lua/plugins/markdown/img-clip.lua
@@ -0,0 +1,13 @@
+return {
+  "HakonHarnes/img-clip.nvim",
+  event = "VeryLazy",
+  opts = {
+    default = {
+      process_cmd = "magick png:- -set gamma 0.454545 png:-",
+    },
+  },
+  keys = {
+    -- suggested keymap
+    { "<leader>p", "<cmd>PasteImage<cr>", desc = "Paste image from system clipboard" },
+  },
+}
