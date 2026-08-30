0db64f6f8066cb63008caf9157891b0e4366cfea
suzuuuuu09 <140299273+suzuuuuu09@users.noreply.github.com>
2026-08-30 15:26:18 +0900

    🔧 chore(nix): enable weekly nh store cleanup

--- .config/nix/home/common/programs/nh.nix
+++ .config/nix/home/common/programs/nh.nix
@@ -2,5 +2,10 @@
   programs.nh = {
     enable = true;
     flake = "${config.home.homeDirectory}/dotfiles";
+    clean = {
+      enable = true;
+      dates = "weekly";
+      extraArgs = "--keep-since 30d --keep-one";
+    };
   };
 }
