d5afd2d518356c5131d18f6baf35d02417792e9a
suzuuuuu09 <140299273+suzuuuuu09@users.noreply.github.com>
2026-09-18 22:31:52 +0900

    🐛 fix(nix): pin suzu UID to 1000

--- .config/nix/hosts/os/default.nix
+++ .config/nix/hosts/os/default.nix
@@ -107,7 +107,7 @@
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.${username} = {
     isNormalUser = true;
-    uid = 1001;
+    uid = 1000;
     description = "suzu";
     extraGroups = [
       "networkmanager"
